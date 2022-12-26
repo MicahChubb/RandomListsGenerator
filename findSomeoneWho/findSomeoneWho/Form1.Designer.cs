fe3b1f17a3aa889efb0f0932983b70dd8aa27549
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2022-12-26 19:20:40 +1100

    Program from computer

---
+++ findSomeoneWho/findSomeoneWho/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace findSomeoneWho
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblNumPlayers = new System.Windows.Forms.Label();
+            this.txtNumPlayers = new System.Windows.Forms.TextBox();
+            this.lblNumWords = new System.Windows.Forms.Label();
+            this.txtNumWords = new System.Windows.Forms.TextBox();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(25, 113);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(127, 29);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate List";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblNumPlayers
+            // 
+            this.lblNumPlayers.AutoSize = true;
+            this.lblNumPlayers.Location = new System.Drawing.Point(21, 15);
+            this.lblNumPlayers.Name = "lblNumPlayers";
+            this.lblNumPlayers.Size = new System.Drawing.Size(131, 20);
+            this.lblNumPlayers.TabIndex = 1;
+            this.lblNumPlayers.Text = "Number of Players";
+            // 
+            // txtNumPlayers
+            // 
+            this.txtNumPlayers.Location = new System.Drawing.Point(158, 12);
+            this.txtNumPlayers.Name = "txtNumPlayers";
+            this.txtNumPlayers.Size = new System.Drawing.Size(56, 27);
+            this.txtNumPlayers.TabIndex = 2;
+            this.txtNumPlayers.Enter += new System.EventHandler(this.validation_Enter);
+            // 
+            // lblNumWords
+            // 
+            this.lblNumWords.AutoSize = true;
+            this.lblNumWords.Location = new System.Drawing.Point(251, 12);
+            this.lblNumWords.Name = "lblNumWords";
+            this.lblNumWords.Size = new System.Drawing.Size(127, 20);
+            this.lblNumWords.TabIndex = 3;
+            this.lblNumWords.Text = "Number of Words";
+            // 
+            // txtNumWords
+            // 
+            this.txtNumWords.Location = new System.Drawing.Point(384, 12);
+            this.txtNumWords.Name = "txtNumWords";
+            this.txtNumWords.Size = new System.Drawing.Size(56, 27);
+            this.txtNumWords.TabIndex = 4;
+            this.txtNumWords.Enter += new System.EventHandler(this.validation_Enter);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(23, 59);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(92, 20);
+            this.lblSource.TabIndex = 6;
+            this.lblSource.Text = "List Location";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(114, 55);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(228, 27);
+            this.txtSource.TabIndex = 7;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(348, 55);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(94, 29);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(25, 145);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 20);
+            this.lblError.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 256);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.txtNumWords);
+            this.Controls.Add(this.lblNumWords);
+            this.Controls.Add(this.txtNumPlayers);
+            this.Controls.Add(this.lblNumPlayers);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Generate Lists";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnGenerate;
+        private Label lblNumPlayers;
+        private TextBox txtNumPlayers;
+        private Label lblNumWords;
+        private TextBox txtNumWords;
+        private Label lblSource;
+        private TextBox txtSource;
+        private Button btnBrowse;
+        private OpenFileDialog openFileDialog1;
+        private Label lblError;
+    }
+}
---
+++ findSomeoneWho/findSomeoneWho/Form1.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace findSomeoneWho
+{
+    public partial class Form1 : Form
+    {
+        // Variables to deal with our input and output of the file
+        static string? path;
+        static string? location;
+        static string? outputFile;
+
+        List<string> wordList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            // Get the input text file from user
+            openFileDialog1.ShowDialog();
+            path = openFileDialog1.FileName;
+
+            // Reset the source textbox
+            txtSource.Text = path;
+            txtSource.BackColor = Color.White;
+
+            // Read text file into our list
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                wordList.Add(line);
+            }
+
+            // Setup our output file, save into the same directory as the source
+            location = Path.GetDirectoryName(path);
+            outputFile = String.Format("{0}/FindSomeoneWho_output.csv", location);
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int numWords;
+            int numPlayers;
+
+            bool words = int.TryParse(txtNumWords.Text, out numWords);
+            bool players = int.TryParse(txtNumPlayers.Text, out numPlayers);
+
+            lblError.Text = "";
+
+            // Validating for numbers
+            if (words == false || players == false)
+            {
+                if (words == false)
+                {
+                    txtNumWords.BackColor = Color.Red;
+                    lblError.Text += "\nNumber of Words is not a Number";
+                }
+                if (players == false)
+                {
+                    txtNumPlayers.BackColor = Color.Red;
+                    lblError.Text += "\nNumber of Players is not a Number";
+                }
+                return;
+            }
+
+            // Validating for a wordlist
+            if(wordList.Count <= 0)
+            {
+                txtSource.BackColor = Color.Red;
+                lblError.Text += "\nNo input list!";
+                return;
+            }
+
+
+            // If we pass the checks, we do the generator thing
+            listGenerator(numWords, numPlayers);
+
+            lblError.Text = "DONE!";
+        }
+
+        private void validation_Enter(object sender, EventArgs e)
+        {
+            // Reset the color to white on focus
+            TextBox? txtValidation = sender as TextBox;
+            txtValidation.BackColor = Color.White;
+            
+        }
+
+        private void listGenerator(int numWords, int numPlayers)
+        {
+            // Setting up our StreamWriter to ouput a file with name and location we specified
+            using (StreamWriter sw = File.CreateText(outputFile))
+            {
+                // Give our csv a header, this makes our mailmerge work better
+                string header = "";
+                for(int k = 1; k <= numWords; k++)
+                {
+                    header += String.Format("Thing {0},", k);
+                }
+                sw.WriteLine(header.Remove(header.Length - 1, 1));
+
+                
+                for (int i = 0; i < numPlayers; i++)
+                {
+                    List<int> previous = new List<int>();
+                    Random rand = new Random();
+
+                    int element;
+                    string list = "";
+
+                    // Make our lists!
+                    for (int j = 0; j < numWords; j++)
+                    {
+                        // Keep randomising until we have a new unique element
+                        while (previous.Contains(element = rand.Next(0, 100)))
+                        {
+
+                        }
+                        // Add the number to the list and then the wordList phrase to our string
+                        previous.Add(element);
+                        list += String.Format("{0},", wordList[element]);
+                    }
+
+                    // Write out our string to the CSV
+                    sw.WriteLine(list.Remove(list.Length - 1, 1));
+                }
+            }
+        }
+    }
+}
